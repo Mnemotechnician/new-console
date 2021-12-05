2db328c2729cc81c34da8b6f9ffd0e91fa43a5d7
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-05 12:32:05 +0300

    Hdhhduduejzjjsk

--- src/newconsole/io/AutorunManager.java
+++ src/newconsole/io/AutorunManager.java
@@ -11,7 +11,7 @@ import mindustry.game.*;
 public class AutorunManager {
 	
 	/** All default event classes */
-	public static Seq<Class>> allEvents = new Seq(50);
+	public static Seq<Class> allEvents = new Seq(50);
 	/** All current events */
 	public static Seq<AutorunEntry> events = new Seq(); 
 	
