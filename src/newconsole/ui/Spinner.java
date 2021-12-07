18bc814a3482227b98b30771ffad54abac9392ff
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-07 18:51:10 +0300

    rawr

--- src/newconsole/ui/Spinner.java
+++ src/newconsole/ui/Spinner.java
@@ -76,7 +76,7 @@ public class Spinner extends TextButton {
 			
 			if (col.getScene() != null) {
 				col.visible = true;
-				col.toFront();
+				//col.toFront(); TODO: causes more harm than sudden disappearing of spinners
 				col.color.a = parentAlpha * color.a;
 				col.setSize(width, col.getPrefHeight());
 				
