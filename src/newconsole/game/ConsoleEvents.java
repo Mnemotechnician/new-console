2af82ad1ef4840909613594a9cdbe15c8bbc1e72
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-31 13:16:19 +0300

    update repo reference

---
+++ src/newconsole/game/ConsoleEvents.java
@@ -0,0 +1,23 @@
+package newconsole.game;
+
+public class ConsoleEvents {
+	
+	/*
+	public static class ConsoleToggleEvent {
+		boolean isOpening;
+		
+		public ConsoleToggleEvent(boolean isOpening) {
+			this.isOpening = isOpening;
+		}
+	}
+	
+	public static class ConsoleRunEvent {
+		public String script;
+		
+		public ConsoleRunEvent(String script) {
+			this.script = script;
+		}
+	}
+	*/
+	
+}
