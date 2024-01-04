ae8e5559da3ca112fb2ef046281edbcb2b5a005a
Djspac3 <149281216+Djspac3@users.noreply.github.com>
2024-01-04 15:19:40 -0500

    Update startup.js NChelp
    changed NCHelp to just say the text not load it

--- assets/console/startup.js
+++ assets/console/startup.js
@@ -26,17 +26,7 @@ const _nativeContains = (array, name) => {
 }
 
 function NCHelp() {
-	let help = readString("console/startup.js-help");
-
-	let b = new StringBuilder();
-	for (method in _interface) {
-		if (!_nativeContains(_defaultMethods, method)) {
-			b.append("NewConsole.[blue]").append(method).append("[];\n");
-		}
-		
-	}
-	
-	println(Strings.format(help, b.toString()));
+	println("[green]#  hello and welcome to new console one of the first consoles that can be publicly used  #\n#  everything will be explaned here  #\n\n    [white]lets start with what you see now\non this side this is the [yellow]logs [white]every log is printed here\n[red][[Warning]the log scroll back is limted tp 30k characters for performance reasons\n\n    [white] on the right side is the console input\nlets start with what it can do with the buttons:\n\nthe run button well runs the current code\n\nthe prev button changes the current code to the previously ran code in the logs\nthe next button well is the same as the prev button just of course the next in the logs if theres nothing next it will clear the code input\n\nthe scripts button well lets you well view/run saves scripts or save scripts\n\nthe files button opens the ingame file browser [red]WARNING DANGEROUS\n\n[white]the events button opens the script autoren menu [[unfinished?]\n\n    if you want to access anything from this mod just use the [blue]NewConsole [white]object\n\n    [yellow]here are the default functions:\n[white]NCHelp() prints this message\nappend(value) add value to the logs [red]doesnt print to ingame logs\n[white]println(value) append just with \n at the start\nbackread() reads the logs and override the output?\n\nimportCls(name) import any (even modded) vlass by its fully-qualified name?")
 }
 
 const append = text => {
